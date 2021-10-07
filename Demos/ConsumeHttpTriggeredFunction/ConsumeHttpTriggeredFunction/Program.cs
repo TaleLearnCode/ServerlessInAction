feace446b5ccbe75ecb4973bf81cffa4c140e715
Chad Green <chadgreen@chadgreen.com>
2021-10-06 21:53:24 -0400

    Adding demo showing how to consume http-triggered function

---
+++ Demos/ConsumeHttpTriggeredFunction/ConsumeHttpTriggeredFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsumeHttpTriggeredFunction
+{
+
+	internal class Program
+	{
+
+		static readonly HttpClient httpClient = new();
+
+		public static async Task Main()
+		{
+			Console.WriteLine("Press any key to start...");
+			Console.ReadKey(true);
+			try
+			{
+				HttpResponseMessage response = await httpClient.GetAsync("https://func-serverlessinaction-use.azurewebsites.net/api/HttpTrigger1?code=LGjgMY3nlNN5QoY8a1nBGdCkBq5R8ao7sy/f01Fjs8/W6vXMa/0X8g==&name=RVNUG");
+				response.EnsureSuccessStatusCode();
+				string responseBody = await response.Content.ReadAsStringAsync();
+				Console.WriteLine(responseBody);
+			}
+			catch (HttpRequestException ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine(ex.Message);
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+		}
+
+	}
+
+}
